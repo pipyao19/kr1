3ac88e2a764d92d0299bf76343a3b2b19ed4eec8
pipyao19 <push535@gmail.com>
2020-12-03 13:50:40 +0900

    ыаввм

---
+++ src/задание3.java
@@ -0,0 +1,9 @@
+public class задание3 {
+    public static void main(String[] args) {
+        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr3.length; i++) {
+            if (arr3[i] < 6) arr3[i] *= 2;
+            System.out.print(arr3[i] + " ");
+        }
+    }
+}
